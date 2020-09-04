bf22191c02f1bc90c6103190e99f691f010ed812
nmm6025 <nmm6025@psu.edu>
2020-09-04 22:10:47 +0000

    syntax

--- main.py
+++ main.py
@@ -98,5 +98,5 @@ elif grade3 == "F" or grade3 == "f":
 else:  
   print(f"Invalid unit({grade3})")
 
-  GPA = ((gradepoint1 * credit1 + gradepoint2 * credit2 + gradepoint3 * credit3) / (credit1 + credit2 + credit3)
+GPA = ((gradepoint1 * credit1 + gradepoint2 * credit2 + gradepoint3 * credit3) / (credit1 + credit2 + credit3)
   print(f"Your GPA is: {GPA}")
